494be7e0439012e1ffab049517948007e82469ad
Adam Farhat <akfarhat05@gmail.com>
2026-08-29 13:59:21 +0200

    Replaced yfinance ticker config with CoinGecko/Alpha Vantage indentifiers

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 DATA_DIR = "data"  # relative folder for downloaded CSVs
-TICKER = "PAXG-USD"  # yfinance ticker for Pax Gold
+COINGECKO_COIN_ID = "pax-gold"  # CoinGecko coin id for Pax Gold
+ALPHAVANTAGE_SYMBOL = "PAXG"  # Alpha Vantage symbol for Pax Gold
+OUTPUT_CSV = "paxg.csv"  # output filename (same regardless of source)
